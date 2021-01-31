64deae223711c5fa33c7e965036d42c88cb61b61
Fillipe Augusto <fillipe484@gmail.com>
2021-01-30 21:26:12 -0300

    my first component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Greetings(){
+  return <h4>Hi this is my first component</h4>
+}
+
+ReactDOM.render(<Greetings/>, document.getElementById('root'))
