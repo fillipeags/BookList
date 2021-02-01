c697059225836bb28f8fdbdda339f742294629a4
Fillipe Augusto <fillipe484@gmail.com>
2021-02-01 20:07:57 -0300

    jsx javascript

--- src/index.js
+++ src/index.js
@@ -7,37 +7,24 @@ function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
+  const title = "Criatvidade S.A";
+  const author = "Ed Catmull, Amy Wallace";
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/617wmdLWVcL._AC_UY218_.jpg"
+        alt="Criatividade SA"
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/617wmdLWVcL._AC_UY218_.jpg"
-    alt="Criatividade SA"
-  />
-);
-
-const Title = () => <h1>Criatividade S.A</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Ed Catmull, Amy Wallace
-  </h4>
-);
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
