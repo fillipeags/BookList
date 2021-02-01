e4c502925115c9485d24f97c4524843ea94d7b47
Fillipe Augusto <fillipe484@gmail.com>
2021-02-01 19:48:54 -0300

    book component and css styling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
+import "./index.css";
+
+function BookList() {
   return (
-    <>
-      <Person />
-      <Message />
-    </>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>Fillipe</h2>;
-const Message = () => {
-  return <h4>Hi this is my first component</h4>;
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
 };
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/617wmdLWVcL._AC_UY218_.jpg"
+    alt="Criatividade SA"
+  />
+);
+
+const Title = () => <h1>Criatividade S.A</h1>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Ed Catmull, Amy Wallace
+  </h4>
+);
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
