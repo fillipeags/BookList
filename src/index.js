c79bf24114d57052fae2133c6da65fefc227c118
Fillipe Augusto <fillipe484@gmail.com>
2021-02-02 10:22:25 -0300

    props

--- src/index.js
+++ src/index.js
@@ -3,26 +3,42 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/617wmdLWVcL._AC_UY218_.jpg",
+  title: "Ed Catmull, Amy Wallace",
+  author: "Criatvidade S.A",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/618qR6-xcQL._AC_UY218_.jpg",
+  title: " Jake Knapp, John Zeratsky",
+  author: "Sprint. O Método Usado no Google ",
+};
+
 function BookList() {
   return (
     <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
       <Book />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "Criatvidade S.A";
-  const author = "Ed Catmull, Amy Wallace";
-
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/617wmdLWVcL._AC_UY218_.jpg"
-        alt="Criatividade SA"
-      />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.author}</h1>
+      <h4>{props.title}</h4>
     </article>
   );
 };
