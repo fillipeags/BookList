bf206505a6b4d6bbf72fe6578fc7c4e6b844f35e
Fillipe Augusto <fillipe484@gmail.com>
2021-02-04 19:01:06 -0300

    images

--- src/Components/BookList/Book.js
+++ src/Components/BookList/Book.js
@@ -10,7 +10,7 @@ const Book = (props) => {
 
   return (
     <article className="book">
-      <img src={img} alt="" />
+      <img src={img} alt=" " />
       <h1>{title}</h1>
       <h4>{author}</h4>
       <button type="button" onClick={clickHandler}>
